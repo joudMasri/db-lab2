327866e7bc7b7dfedb0c4c81b2207118eed36bb8
Joud Masri <joudmasri679@gmail.com>
2023-03-03 17:09:09 +0200

    first commit

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxSupplier = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prices_category = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.prices_category)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxSupplier
+            // 
+            this.comboBoxSupplier.FormattingEnabled = true;
+            this.comboBoxSupplier.Location = new System.Drawing.Point(915, 94);
+            this.comboBoxSupplier.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxSupplier.Name = "comboBoxSupplier";
+            this.comboBoxSupplier.Size = new System.Drawing.Size(223, 24);
+            this.comboBoxSupplier.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(912, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please Choose a Supplier";
+            // 
+            // prices_category
+            // 
+            this.prices_category.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.prices_category.Location = new System.Drawing.Point(915, 199);
+            this.prices_category.Name = "prices_category";
+            this.prices_category.RowHeadersWidth = 51;
+            this.prices_category.RowTemplate.Height = 24;
+            this.prices_category.Size = new System.Drawing.Size(304, 268);
+            this.prices_category.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(912, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Product Total Prices by categories";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 554);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.prices_category);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxSupplier);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.prices_category)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSupplier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView prices_category;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using dotnetCHARTING.WinForms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string sqlServer = "DESKTOP-CG06B1F\\INSTANCE_2K19";
+            string dbName = "Northwind";
+            dl = new DataLayer(sqlServer, dbName);
+        }
+
+    
+        DataLayer dl;
+        Chart chart = new Chart();
+        
+        DataTable SuppliersDt, ProductTotalPricesByCategoriesDT;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if (dl.IsValid)
+            {
+                string sql = "SELECT SupplierId, CompanyName FROM Suppliers";
+                SuppliersDt = dl.GetData(sql, "Suppliers");
+                comboBoxSupplier.DataSource = SuppliersDt;
+                comboBoxSupplier.DisplayMember = "CompanyName";
+                comboBoxSupplier.ValueMember = "SupplierId";
+                comboBoxSupplier.SelectedIndexChanged += comboBoxSupplier_SelectedIndexChanged;
+                
+                chart.Dock = DockStyle.None;
+                chart.Use3D = true;
+                chart.Type = ChartType.ComboHorizontal;
+                chart.AutoSize = false;
+                Controls.Add(chart);
+                SupplierChanged();
+            }
+            
+        }
+        private void SupplierChanged()
+        {
+            string query = $@"
+                select 
+	                c.CategoryName,
+	                sum(p.UnitsInStock*p.UnitPrice) as TotalPrice
+                from products as p
+                join Suppliers as s
+                    on s.SupplierID = p.SupplierID
+                join Categories as c
+                    on c.CategoryID = p.CategoryID
+                where s.SupplierID = {comboBoxSupplier.SelectedValue}
+                group by c.CategoryName
+            ";
+            ProductTotalPricesByCategoriesDT = dl.GetData(query, "Product total prices by categories");
+            prices_category.DataSource = ProductTotalPricesByCategoriesDT;
+
+            this.Controls.Remove(chart);
+            this.Controls.Add(chart);
+
+            chart.SeriesCollection.Clear();
+            foreach (DataRow dr in ProductTotalPricesByCategoriesDT.Rows)
+            {
+
+                Element element = new Element(dr["CategoryName"].ToString(), double.Parse(dr["TotalPrice"].ToString()));
+                element.Annotation = new Annotation(dr["TotalPrice"].ToString());
+                ElementCollection elementCollection = new ElementCollection();
+                elementCollection.Add(element);
+                Series serie = new Series(dr["CategoryName"].ToString(), elementCollection);
+                chart.SeriesCollection.Add(serie);
+            }
+        }
+
+        private void comboBoxSupplier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SupplierChanged();
+        }
+
+    }
+}
